ecdf6f0568219d15e5ddae8293e5fb19d78275d0
Anjie Fang <a.fang.1@research.gla.ac.uk>
2017-01-23 10:31:48 +0000

    new

--- beta_timed_data.py
+++ beta_timed_data.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.stats import beta
 
-# creat data
-data = np.random.beta(4, 3, size=(1000))
-data = sorted(data)
 
+def consf(theta, x, numFold, numOfBatch, partition):
+    hist, bins = np.histogram(x, )
