f1eb3df9d06a2114aeed7127097259550055de82
Anjie Fang <a.fang.1@research.gla.ac.uk>
2017-01-24 16:49:25 +0000

    write verify.py

--- costFunction.py
+++ costFunction.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def consfun(theta, x, fold_num, partition_num=100):
+def consfun(theta, x, fold_num=5, partition_num=100):
     # x: given data
     # numFold: the number of fold given a data
     # partition_num: the number of partition used to calculate the area
@@ -13,44 +13,49 @@ def consfun(theta, x, fold_num, partition_num=100):
 
     # estimated p and grad per fold
     est_a = np.zeros(fold_num)
-    est_p = np.zeros(fold_num)
     gradFold = np.zeros([fold_num, 2])
 
     # calculate grad
-    for i in range(fold_num - 1):
+    for i in range(fold_num):
         lenOfpartition = (bins[i+1] - bins[i]) / partition_num
         sampled_x = np.arange(bins[i], bins[i+1],lenOfpartition)
-        sampled_x = sampled_x[:, np.newaxis]
-        est_a[i] = np.dot(np.repeat(lenOfpartition, partition_num), betaPDFForVec(sampled_x[1:], theta))[0]
-        gradFold[i,0] = np.dot(np.repeat(lenOfpartition, partition_num), calculateFirstGrad(sampled_x[1:], theta))[0]
-        gradFold[i,1] = np.dot(np.repeat(lenOfpartition, partition_num), calculateSecondGrad(sampled_x[1:], theta))[0]
+        assert partition_num == len(sampled_x)
+        est_a[i] = np.dot(np.repeat(lenOfpartition, partition_num), betaPDFForVec(np.copy(sampled_x), theta)[:, np.newaxis])[0]
+        # print 'est_a[i]:' + str(est_a[i])
+        gradFold[i,0] = np.dot(np.repeat(lenOfpartition, partition_num), calculateFirstGrad(np.copy(sampled_x), theta)[:, np.newaxis])[0]
+        # print 'gradFold[i,0]:' + str(gradFold[i,0])
+        gradFold[i,1] = np.dot(np.repeat(lenOfpartition, partition_num), calculateSecondGrad(np.copy(sampled_x), theta)[:, np.newaxis])[0]
+        # print 'gradFold[i,1]:' + str(gradFold[i,1])
+
 
     total_est_a = est_a.sum()
     est_p = est_a / total_est_a
-    J = 0.5*(1/fold_num)*np.sum(np.abs(est_p-ps)^2)
 
+    J = 0.5*(1.0/fold_num)*np.sum((est_p-ps)**2)
 
-    # need to be confirmed
     trueGradFold = np.zeros([fold_num, 2])
     trueTotalGrad = np.sum(gradFold, axis=0)
     for i in range(fold_num - 1):
-        trueGradFold[i, 0] = (1 / fold_num) * (est_p[i] - ps[i]) \
-                             * (gradFold[i, 0] * total_est_a - trueTotalGrad[0] * est_a[i]) / total_est_a ^ 2
-        trueGradFold[i, 1] = (1 / fold_num) * (est_p[i] - ps[i]) \
-                             * (gradFold[i, 1] * total_est_a - trueTotalGrad[1] * est_a[i]) / total_est_a ^ 2
+        trueGradFold[i, 0] = (1.0 / fold_num) * (est_p[i] - ps[i]) \
+                             * (gradFold[i, 0] * total_est_a - trueTotalGrad[0] * est_a[i]) / total_est_a ** 2
+
+        trueGradFold[i, 1] = (1.0 / fold_num) * (est_p[i] - ps[i]) \
+                             * (gradFold[i, 1] * total_est_a - trueTotalGrad[1] * est_a[i]) / total_est_a ** 2
 
     GradTheta = np.sum(trueGradFold, axis=0)
     return [J, GradTheta]
 
 def betaPDFForVec(x, theta):
     for i in range(len(x)):
-        x[i] = x[i]^(np.exp(theta[0]-1))*(1-x[i])^(np.exp(theta(1)-1))
+        x[i] = x[i]**(np.exp(theta[0]-1))*(1-x[i])**(np.exp(theta[1]-1))
     return x
 
 def calculateFirstGrad(x, theta):
     for i in range(len(x)):
-        x[i] = x[i]^(np.exp(theta[0])-1)*(1-x[i])^(np.exp(theta[1])-1)*np.log(x[i]) * np.exp(theta[0])
+        x[i] = x[i]**(np.exp(theta[0])-1)*(1-x[i])**(np.exp(theta[1])-1)*np.log(x[i]) * np.exp(theta[0])
+    return x
 
 def calculateSecondGrad(x, theta):
     for i in range(len(x)):
-        x[i] = x[i]^(np.exp(theta[0])-1)*(1-x[i])^(np.exp(theta[1])-1)*np.log(1-x[i]) * np.exp(theta[1])
+        x[i] = x[i]**(np.exp(theta[0])-1)*(1-x[i])**(np.exp(theta[1])-1)*np.log(1-x[i]) * np.exp(theta[1])
+    return x
---
+++ verify.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.stats import beta
+import costFunction
+
+a = 4
+b = 3
+fold_num = 10
+partition = 100
+data = np.random.beta(a,b,20000)
+data = sorted(data)
+
+numPerFold, bins = np.histogram(data, bins=fold_num, density=False)
+numPerFold = numPerFold.astype(float)
+cum_num = np.cumsum(numPerFold)
+
+
+
+for i in range(fold_num):
+    x = np.array(data[:int(cum_num[i])])
+
+    print 'Fold: ' + str(i)
+    [J, grad] = costFunction.consfun([0.5,1], x, partition_num=partition*(i+1))
+    print 'J:' + str(J) + ', Grad:' + str(grad)
+
+    [J1, _] = costFunction.consfun([0.5 + 1e-6, 1], x, partition_num=partition * (i + 1))
+    [J2, _] = costFunction.consfun([0.5 - 1e-6, 1], x, partition_num=partition * (i + 1))
+    print 'J1-J2: ' + str((J1 - J2) / (2e-6))
+    [J3, _] = costFunction.consfun([0.5, 1 + 1e-6], x, partition_num=partition * (i + 1))
+    [J4, _] = costFunction.consfun([0.5, 1 - 1e-6], x, partition_num=partition * (i + 1))
+    print 'J3-J4: ' + str((J3 - J4) / (2e-6))
+
+    exit(-1)
