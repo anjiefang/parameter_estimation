ec23f8fd20b7cb091f2dbad6156b3ab97fe8aa4e
Anjie Fang <a.fang.1@research.gla.ac.uk>
2017-02-04 15:19:36 +0000

    v1.9

--- generate_task.py
+++ generate_task.py
@@ -1,6 +1,6 @@
 size = [1000, 5000, 10000, 15000, 20000, 25000, 30000]
 pars = [[0.3, 0.4], [5.0, 1.0], [2.0, 10.0], [7.0, 8.0]]
-noise = [0.05, 0.1, 0.2, 0.5]
+noise = [0.05, 0.1, 0.2]
 
 script = '$1'
 output = '$2'
@@ -13,7 +13,8 @@ for s in size:
         command = 'python ' + script + \
                   ' -R 10 -size ' + str(s) + \
                   ' -A ' + str(par[0]) + ' -B ' + str(par[1]) + \
-                  ' -o ' + output + ' -sample'
+                  ' -o ' + output
+                  # + ' -sample'
 
         command = 'echo ' + command + ' | submitAll.pl -l walltime=03:59:00'
         f.write(command)
